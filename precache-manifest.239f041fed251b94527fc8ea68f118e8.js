ee71db9ffa7af1fbd516dd51e279be8e7bc2a67f
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-02-06 22:29:39 -0800

    Updates

---
+++ precache-manifest.239f041fed251b94527fc8ea68f118e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a61fef25d0130f62eca17546219fd2",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "911bfa236f825303b585",
+    "url": "/react-project/static/css/main.5851e034.chunk.css"
+  },
+  {
+    "revision": "d0f84f33611fcbf581d3",
+    "url": "/react-project/static/js/2.4135fd03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-project/static/js/2.4135fd03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911bfa236f825303b585",
+    "url": "/react-project/static/js/main.1729d51f.chunk.js"
+  },
+  {
+    "revision": "95dd78137eff9d9db84e",
+    "url": "/react-project/static/js/runtime-main.194fc871.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
